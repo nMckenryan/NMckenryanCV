0404b4b633c4bdd0a95bd3356ab577e1e53acab6
Nigel Mckenzie-Ryan <n.mckenryan@gmail.com>
2021-03-01 12:45:25 +1100

    Init Commit
    Getting CV working on Github Pages

---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": (preferDefault(require("C:\\Users\\User\\Documents\\GitHub\\nigelMckenryanCV\\.cache\\caches\\gatsby-plugin-offline\\app-shell.js"))),
+  "component---src-pages-404-js": (preferDefault(require("C:\\Users\\User\\Documents\\GitHub\\nigelMckenryanCV\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": (preferDefault(require("C:\\Users\\User\\Documents\\GitHub\\nigelMckenryanCV\\src\\pages\\index.js")))
+}
+
